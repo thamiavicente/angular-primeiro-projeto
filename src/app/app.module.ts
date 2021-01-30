effa43a20f36f7956458f90bd41215854348c77b
Thami A. Vicente <thamiavicente@gmail.com>
2021-01-30 08:00:56 -0300

    reorganizando componentes em shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeComponent } from './home/home.component';
-import { MemesLinguagensComponent } from './memes-linguagens/memes-linguagens.component';
-import { FolhetoMercadoComponent } from './folheto-mercado/folheto-mercado.component';
+import { MemesLinguagensComponent } from './shared/components/memes-linguagens/memes-linguagens.component';
+import { FolhetoMercadoComponent } from './shared/components/folheto-mercado/folheto-mercado.component';
 
 registerLocaleData(localeFr, 'pt');
 
