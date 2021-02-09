c732bb1e6d0b5a2d33ff9ea025f8a89381949d18
Thami Vicente <tvicente@aatb.com.br>
2021-02-09 18:06:37 -0300

    Exercício validação de formulário

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form){
+    if (form.invalid) {
+      form.controls.email.markAsTouched();
+      form.controls.password.markAsTouched();
+      console.log(form);
+      return
+    }
+  }
 
+  exibeErro(nomeControle: string, form: FormGroup) {
+    if(!form.controls[nomeControle]){
+      return false;
+    }
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
   }
 
 }
